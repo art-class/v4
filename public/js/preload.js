a531e3dd777fddfd43df9a53c4f45e01d7e911b8
Parrot <knowing.discos0e@icloud.com>
2023-09-04 01:43:59 +0000

    google analytics

--- public/js/preload.js
+++ public/js/preload.js
@@ -43,3 +43,14 @@ hover.href = "https://cdn.jsdelivr.net/gh/ianlunn/hover/css/hover-min.css"
 hover.rel = "stylesheet"
 document.head.appendChild(hover)
 
+// Google Analytics
+var googleAnalytics = document.createElement("script")
+googleAnalytics.src = "https://www.googletagmanager.com/gtag/js?id=G-66ZE075DLD"
+googleAnalytics.async = true
+document.head.appendChild(googleAnalytics)
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-66ZE075DLD');
