40ae2de059b6efdaa9dbef07db900f4465ad71b1
proudparrot2 <hi@proudparrot2.tech>
2023-12-20 07:00:40 -0600

    arc script broke stuff

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,6 @@
 importScripts('/uv/uv.bundle.js');
 importScripts('/uv/uv.config.js');
 importScripts(__uv$config.sw || '/uv/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js')
 
 const sw = new UVServiceWorker();
 
