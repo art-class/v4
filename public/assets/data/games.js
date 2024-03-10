b5f51924f61ae14137365b68bf88979a4d5396a6
proudparrot2 <hi@proudparrot2.tech>
2024-03-10 15:36:53 -0500

    fix a LOT of game urls

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -7,14 +7,14 @@ var games = [
   {
     id: 'idlebreakout',
     title: 'Idle Breakout',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/5cb61b353cff0365c082c9309e82bb561c027201/idle-breakout/index.html/',
+    url: 'https://assets.3kh0.net/idle-breakout/index.html/',
     image: 'https://play-lh.googleusercontent.com/e5jzYTn7Mjh9Ghn-DFpVedkOlRfSOyCS2AFgXdTrADIuuUbjx6vP6r_YOM2j5m7uLQ'
   },
   {
     id: 'eggycar',
     title: 'Eggy Car',
-    url: 'https://raw.githack.com/3kh0/3kh0-Assets/main/eggycar/index.html',
-    image: 'https://raw.githubusercontent.com/3kh0/3kh0-assets/main/eggycar/eggy-car.png'
+    url: 'https://assets.3kh0.net/eggycar/index.html',
+    image: 'https://assets.3kh0.net/eggycar/eggy-car.png'
   },
   {
     id: 'rocketleague',
@@ -38,7 +38,7 @@ var games = [
   {
     id: 'thereisnogame',
     title: 'There Is No Game',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/there-is-no-game/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/there-is-no-game/index.html',
     image: 'https://play-lh.googleusercontent.com/a6DfrYUL6viV0LAZFplqqA44gE0LHE0PyDnKHgdlydiUvccwzSBWrmRQSx5cjdL74Us=w240-h480-rw'
   },
   {
@@ -64,7 +64,7 @@ var games = [
   {
     id: 'cuttherope',
     title: 'Cut the Rope',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/ctr/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/ctr/index.html',
     image: 'https://play-lh.googleusercontent.com/8FNcAyLXtQB_0Ux2ZO8VZoEoihL6a5VMBNf6V2lydRM24hXLnNUdlEup1d5miVjl3JY'
   },
   {
@@ -77,13 +77,13 @@ var games = [
     id: 'backrooms',
     title: 'Backrooms',
     description: 'Scary game based on the viral 4Chan post.',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/60f2f97b00ffe9566ee06c82ecaf67255931118b/backrooms/index.html',
+    url: 'https://assets.3kh0.net/60f2f97b00ffe9566ee06c82ecaf67255931118b/backrooms/index.html',
     image: 'https://media.wired.com/photos/627b0c1fb6048c47d506c6c0/1:1/w_679,h_679,c_limit/Backrooms-Games.jpg'
   },
   {
     id: 'youarebezos',
     title: 'You are Bezos',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/you-are-bezos/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/you-are-bezos/index.html',
     image: 'https://img.itch.zone/aW1nLzE1NzI2NjIucG5n/original/Equhiy.png'
   },
   {
@@ -107,14 +107,14 @@ var games = [
   {
     id: 'jetpackjoyride',
     title: 'Jetpack Joyride',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/21ac7257444af1713884b35045938d57e88a4993/jetpack-joyride/index.html',
+    url: 'https://assets.3kh0.net/21ac7257444af1713884b35045938d57e88a4993/jetpack-joyride/index.html',
     image: 'https://play-lh.googleusercontent.com/Pl2nmu5U9CH3NQqLBTjnokoV325zESrX6KIaHrwSqREEJDWIv1yJpRCXzl9r3oMnEWHC',
   },
   {
     id: 'shellshockers',
     title: 'Shell Shockers',
     description: 'Shell Shockers is unique online shooter in first-person perspective where all the characters are represented exclusively by eggs.',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/shellshockers/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/shellshockers/index.html',
     image: 'https://play-lh.googleusercontent.com/P-nde227L29s8w5U44kTPLiEnMEJUhJpEr4jL6tD6LV65Xz0JZtI4wEyFN-smsNrx-Q'
   },
   {
@@ -133,7 +133,7 @@ var games = [
   {
     id: 'motox3m',
     title: 'MotoX3M',
-    //url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/motox3m/index.html',
+    //url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/motox3m/index.html',
     url: 'https://art-class.github.io/assets/motox3m',
     image: 'https://play-lh.googleusercontent.com/AbQMuT4ncBfWDqNF-4ubmfaRag59nrW3fJARDsHK51_PENScmWCnMFeQX81wfPlfGBo'
   },
@@ -141,19 +141,19 @@ var games = [
     id: 'thefinalearth',
     title: 'The Final Earth',
     description: 'Earth has been destroyed. It\'s a good thing you teleported to a new planet, also known as The Final Earth. Gather resources and build a colony',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/the-final-earth/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/the-final-earth/index.html',
     image: 'https://img.itch.zone/aW1nLzIyMTU1NDAucG5n/original/6vGlZe.png'
   },
   {
     id: 'fnf',
     title: 'Friday Night Funkin',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/21ac7257444af1713884b35045938d57e88a4993/fridaynightfunkin/index.html',
+    url: 'https://assets.3kh0.net/21ac7257444af1713884b35045938d57e88a4993/fridaynightfunkin/index.html',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Boyfriend-2.png/250px-Boyfriend-2.png'
   },
   {
     id: 'paperio2',
     title: 'Paper.io 2',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/21ac7257444af1713884b35045938d57e88a4993/paperio2/index.html',
+    url: 'https://assets.3kh0.net/21ac7257444af1713884b35045938d57e88a4993/paperio2/index.html',
     image: 'https://play-lh.googleusercontent.com/KxcjUrs8S75iOPnKBR2b-s7eGtqbREwLgzttVxaM9Znl5ZxsDCl6qEZHny4rBkMeYA=w240-h480-rw'
   },
   {
@@ -165,12 +165,12 @@ var games = [
   {
     id: 'ducklife4',
     title: 'Duck Life 4',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/21ac7257444af1713884b35045938d57e88a4993/ducklife4/index.html',
+    url: 'https://assets.3kh0.net/21ac7257444af1713884b35045938d57e88a4993/ducklife4/index.html',
     image: 'https://play-lh.googleusercontent.com/AUGkRrKMtUCLVE9vCvlfMY9Ny9EBqzx17yejVtEEhvpkw-H6lJlvBHgCMqPJm8HV_tM'
   },
   {
     id: 'retrobowl',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/21ac7257444af1713884b35045938d57e88a4993/retro-bowl/index.html',
+    url: 'https://assets.3kh0.net/21ac7257444af1713884b35045938d57e88a4993/retro-bowl/index.html',
     title: 'Retro Bowl',
     image: 'https://play-lh.googleusercontent.com/WRM5Y1xZmzcCP1YtO5zl6G2g7CU5c5ZfjX4UVrgi1bpNgkfy-wuB-bQx3kkeRfaGYQ'
   },
@@ -183,19 +183,19 @@ var games = [
   {
     id: 'templerun2',
     title: 'Temple Run 2',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/21ac7257444af1713884b35045938d57e88a4993/temple-run-2/index.html',
+    url: 'https://assets.3kh0.net/21ac7257444af1713884b35045938d57e88a4993/temple-run-2/index.html',
     image: 'https://play-lh.googleusercontent.com/go4XqS4mYs-G2tZymiVLF4wJYXIi5QrvwixNRzssk4G_vRBHrAdg4E1ddNwy9c2cZA'
   },
   {
     id: 'stickmanhook',
     title: 'Stickman Hook',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/stickman-hook/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/stickman-hook/index.html',
     image: 'https://play-lh.googleusercontent.com/yXqBI1Vb1b-xjwmXLE2T7FQbgHIxfvjze1b6jO2aLHfy8Y8ta7NFSEVqZoPHtM2Ajyrl'
   },
   {
     id: 'run3',
     title: 'Run 3',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/run%203/Run%203.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/run%203/Run%203.html',
     image: 'https://play-lh.googleusercontent.com/A97ygyZMgnoapGfPhWZn9QBMsrskPuPQmJqeEtROZWnbQRfAOZZlhtP-UBWxaKtMI6U',
     description: 'Run 3 is a classic game where you swerve through space in a race to the finish. Play hundreds of new levels in this fast-paced platformer'
   },
@@ -217,57 +217,57 @@ var games = [
     id: 'justfall',
     title: 'Just Fall',
     description: 'Just Fall is a game similar to Fall Guys, where you compete in minigames to be the sole winner',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/just-fall/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/just-fall/index.html',
     image: 'https://play-lh.googleusercontent.com/-0rSllZ8as88Jcs3iZ7-JCFyTb3YLB-HjR7bY7P6q_2MAswmDH4h1qNPfFiaZqzkkg'
   },
   {
     id: 'sm64',
     title: 'Super Mario 64',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/sm64/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/sm64/index.html',
     image: 'https://assets1.ignimgs.com/2019/05/31/mario-64---button-1559263987447.jpg',
     description: 'Controls: <br>Movement: Arrow Keys <br>A: X <br>B: C <br>R: Q <br>Z: Space <br>Start: Enter <br>C-stick: WASD<br><br>You might need to go into fullscreen for the keybinds to register'
   },
   {
     id: 'snowball',
     title: 'snowball.io',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/snowbattle/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/snowbattle/index.html',
     image: 'https://play-lh.googleusercontent.com/X8FEOENa_t5mlRllBh2RRLc6sMnPIq56Wxa2ndValDOmwzIRhAv7MG6fT97TLQ2DK48'
   },
   {
     id: 'minecraft',
     title: 'Minecraft Classic',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/21ac7257444af1713884b35045938d57e88a4993/minecraft-classic/index.html',
+    url: 'https://assets.3kh0.net/21ac7257444af1713884b35045938d57e88a4993/minecraft-classic/index.html',
     image: 'https://cdn.iconscout.com/icon/free/png-256/minecraft-2752120-2284937.png'
   },
   {
     id: 'smashkarts',
     title: 'Smash Karts',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d108460237587095c92fe0968f1595f30cf5a5cc/smashkarts/index.html',
+    url: 'https://assets.3kh0.net/d108460237587095c92fe0968f1595f30cf5a5cc/smashkarts/index.html',
     image: 'https://geometry-dash.co/upload/imgs/smash-karts-logo.png'
   },
   {
     id: 'tunnelrush',
     title: 'Tunnel Rush',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e00403015397cb588f42d3a461b5ac9503dd7c2d/tunnel-rush/index.html',
+    url: 'https://assets.3kh0.net/e00403015397cb588f42d3a461b5ac9503dd7c2d/tunnel-rush/index.html',
     image: 'https://images.hellokids.com/_uploads/_tiny_galerie/20180415/vign-tunnel-rush-hk-w8v_u89.jpg',
   },
   {
     id: 'awesometanks2',
     title: 'Awesome Tanks 2',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/60f2f97b00ffe9566ee06c82ecaf67255931118b/awesometanks2/index.html',
+    url: 'https://assets.3kh0.net/60f2f97b00ffe9566ee06c82ecaf67255931118b/awesometanks2/index.html',
     image: 'https://play-lh.googleusercontent.com/fiH2wSovQkxQ2dQViDAphNBbM0B1dYxTlJqnYwKXeSoQQZbBNkZZuLyMkv4gglb7LH4'
   },
   {
     id: 'fireandice',
     title: 'A Dance of Fire and Ice',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/a-dance-of-fire-and-ice/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/a-dance-of-fire-and-ice/index.html',
     image: 'https://play-lh.googleusercontent.com/L8rCly8hMWuP95PbihDZlSV2u0oGrs0hl2lEXboO7XHX2XalJZ1rjgOsFYXnEDWbpQ',
     description: 'A Dance of Fire and Ice is a simple one-button rhythm game. Press on every beat of the music to move in a line. Every pattern has its own rhythm to it. It can get difficult. This game is purely based on rhythm, so use your ears more than your sight.'
   },
   {
     id: 'escapeprison',
     title: 'Henry Stickmin: Escaping the Prison',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/escapingtheprison/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/escapingtheprison/index.html',
     image: 'https://play-lh.googleusercontent.com/MKW0yzbpA-faDMztP2xkirlXsHwL7QutC_9VyQlbr4VTbPOyCZInL7zYG9EA4XpX4mtM'
   },
   {
@@ -297,7 +297,7 @@ var games = [
   {
     id: 'baldisbasics',
     title: 'Baldi\'s Basics',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/60f2f97b00ffe9566ee06c82ecaf67255931118b/baldis-basics/index.html',
+    url: 'https://assets.3kh0.net/60f2f97b00ffe9566ee06c82ecaf67255931118b/baldis-basics/index.html',
     image: 'https://play-lh.googleusercontent.com/EPV1TB4So1lB0DGrdCVExDpNU8ML67nd8OqBeoOIM-s6sDicxmDdPvCXD6n7LKevFl0'
   },
   {
@@ -309,25 +309,25 @@ var games = [
   {
     id: 'crossyroad',
     title: 'Crossy Road',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/crossyroad/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/crossyroad/index.html',
     image: 'https://cdn.jim-nielsen.com/ios/512/crossy-road-endless-arcade-2014-12-02.png'
   },
   {
     id: 'subwaysurfers',
     title: 'Subway Surfers',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/subway-surfers/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/subway-surfers/index.html',
     image: 'https://cdnb.artstation.com/p/assets/images/images/040/799/939/large/celine-j-1-74-shanghai-icon-1024-x.jpg?1629908071'
   },
   {
     id: 'geometrydash',
     title: 'Geometry Dash',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/21ac7257444af1713884b35045938d57e88a4993/geodash/index.html',
+    url: 'https://assets.3kh0.net/21ac7257444af1713884b35045938d57e88a4993/geodash/index.html',
     image: 'https://upload.wikimedia.org/wikipedia/en/3/35/Geometry_Dash_Logo.PNG'
   },
   {
     id: 'idlecraft',
     title: 'Idlecraft',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/21ac7257444af1713884b35045938d57e88a4993/grindcraft/index.html',
+    url: 'https://assets.3kh0.net/21ac7257444af1713884b35045938d57e88a4993/grindcraft/index.html',
     image: 'https://play-lh.googleusercontent.com/Pl3YYYdxM1Lhe-pXFwaLfmzSpqDZqlzvxS7-BH60Mho_cydWeOCkhTCrSgtMEXL54Cm0'
   },
   {
@@ -339,19 +339,19 @@ var games = [
   {
     id: 'fruitninja',
     title: 'Fruit Ninja',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/fruitninja/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/fruitninja/index.html',
     image: 'https://play-lh.googleusercontent.com/iRlGmvtJ524J-gAGdJJchMv-MH-9AuG80TAx8Rv8q3lObiI7kLVZrEvjVOM-yw3BBH4'
   },
   {
     id: 'bloxors',
     title: 'Bloxors',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/60f2f97b00ffe9566ee06c82ecaf67255931118b/bloxors/index.html',
+    url: 'https://assets.3kh0.net/60f2f97b00ffe9566ee06c82ecaf67255931118b/bloxors/index.html',
     image: 'https://play-lh.googleusercontent.com/FHpA3r3FHDzVNtBpjpi9Zq8JA81bvRXBEbdkBvL3COOucsMfn_fYYhEFE5g55VkeSQ'
   },
   {
     id: 'chromedino',
     title: "Chrome Dino Game",
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/chrome-dino/index.html',
+    url: 'https://assets.3kh0.net/e3144855e924629d8f7c41238660a823a5336705/chrome-dino/index.html',
     image: 'https://qph.cf2.quoracdn.net/main-qimg-c803e5912e045867b4e2f033c1b57ff9-lq'
   },
   {
@@ -369,19 +369,19 @@ var games = [
   {
     id: 'basketballstars',
     title: 'Basketball Stars',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/basketball-stars/index.html',
+    url: 'https://assets.3kh0.net/d1a97cbddcf69b15adf2ab4a491df717ecf22938/basketball-stars/index.html',
     image: 'https://play-lh.googleusercontent.com/EjJV6kCXgX9EIhKEtpYhQF8-BUb5En8sDKpOPiWSQJUxv9_RAfl4tMxyIMkQYgeqC6I=w240-h480-rw'
   },
   {
     id: 'amongus',
     title: 'Among Us',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/among-us/index.html',
+    url: 'https://assets.3kh0.net/d1a97cbddcf69b15adf2ab4a491df717ecf22938/among-us/index.html',
     image: 'https://play-lh.googleusercontent.com/8ddL1kuoNUB5vUvgDVjYY3_6HwQcrg1K2fd_R8soD-e2QYj8fT9cfhfh3G0hnSruLKec'
   },
   {
     id: 'rocketleague',
     title: 'Rocket League',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/Rocket-League/index.html',
+    url: 'https://assets.3kh0.net/d1a97cbddcf69b15adf2ab4a491df717ecf22938/Rocket-League/index.html',
     image: 'https://cdn2.iconfinder.com/data/icons/popular-games-1/50/rocketleague_squircle-512.png'
   },
   {
@@ -393,13 +393,13 @@ var games = [
   {
     id: 'bitlife',
     title: 'Bitlife',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/bitlife/index.html',
+    url: 'https://assets.3kh0.net/d1a97cbddcf69b15adf2ab4a491df717ecf22938/bitlife/index.html',
     image: 'https://d.newsweek.com/en/full/1317374/bitlife-ribbons-how-get-all-complete-list-android-ios-cheats-guide-tips-become.jpg?w=1600&h=1600&q=88&f=75befc746fb83a0c568c44ca07bc5e64'
   },
   {
     id: 'btd4',
     title: 'Bloons TD 4',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/btd4/index.html',
+    url: 'https://assets.3kh0.net/d1a97cbddcf69b15adf2ab4a491df717ecf22938/btd4/index.html',
     image: 'https://assets-prd.ignimgs.com/2022/08/16/bloons-td4-button-1660612386362.jpg'
   },
   {
@@ -411,19 +411,19 @@ var games = [
   {
     id: 'dogeminer',
     title: "Doge Miner",
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/21ac7257444af1713884b35045938d57e88a4993/DogeMiner/index.html',
+    url: 'https://assets.3kh0.net/21ac7257444af1713884b35045938d57e88a4993/DogeMiner/index.html',
     image: 'https://play-lh.googleusercontent.com/7diC1RZgf6Z5XCspiwJMYD3vQz5TmjFElLw5qnPJZjcjNXgfhepPJzmIBijgraJRd8uv'
   },
   {
     id: 'aquapark',
     title: 'aquapark.io',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/21ac7257444af1713884b35045938d57e88a4993/aquapark-slides/index.html',
+    url: 'https://assets.3kh0.net/21ac7257444af1713884b35045938d57e88a4993/aquapark-slides/index.html',
     image: 'https://play-lh.googleusercontent.com/pNI21ILS4I9IAzveYzjbfk5D5LGuL03b0Ea4OtlFRLD_yk7KYLBeLlBmsvhH7Uj-dQn6'
   },
   {
     id: '2048',
     title: '2048',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/f839d26b4770ba137c399dc1a1c6ccf22e5f3565/2048/index.html',
+    url: 'https://assets.3kh0.net/f839d26b4770ba137c399dc1a1c6ccf22e5f3565/2048/index.html',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/2048_logo.svg/220px-2048_logo.svg.png'
   },
   {
